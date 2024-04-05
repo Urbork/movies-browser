a881526c405339184efabff098f26f7536577c0f
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-04-05 17:18:07 +0200

    removed unnecessary comment

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -10,8 +10,6 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    //padding: 16px;
-    // padding do usunięcia, komentarz dla informacji 
     background-color: ${({ theme }) => theme.color.whisper};
     font-family: "Poppins", "sans-serif";
     color: ${({ theme }) => theme.color.black};
