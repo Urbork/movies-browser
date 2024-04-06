f82b6c96e0866a8a4ca6a95a13f8db1c2a135c06
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-04-07 00:58:21 +0200

    some new colors and font weights

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,29 @@
 export const theme = {
+  breakpoint: {
+    phone: 590,
+    mobile: 712,
+    descriptionChange: 1023,
+    tablet: 1023,
+  },
+  fontWeight: {
+    regular: 400,
+    semibold: 500,
+    medium: 600,
+    semiBold: 700,
+    bold: 800,
+  },
   color: {
-    white: "#ffffff",
-    whisper: "#f5f5fa",
-    mystic: "#e4e6e0",
+    white: "#FFFFFF",
+    imageBG: "#C4C4C4",
+    tagBG: "#E4E6F0",
+    darkerGrey: "#7E839A",
+
+    heatherTransparent: "#BAC7D580",
+    whisper: "#F5F5FA",
+    mystic: "#E4E6E0",
     waterloo: "#7E839A",
     codGray: "#181818",
+
     black: "#000000",
   },
 };
