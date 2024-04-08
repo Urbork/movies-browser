ef17c5fe1acaf6ce714d45701f47fa06bc2e6061
Jonasz Jachym <jajonaszjj@gmail.com>
2024-04-08 20:59:37 +0200

    Rating display changed and margins updated

--- src/features/MoviesList/styled.js
+++ src/features/MoviesList/styled.js
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
 export const MoviesListSection = styled.section`
-  margin-top: 150px;
+  margin-top: 56px;
   max-width: 1368px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    margin: 0 16px;
+    margin: 24px 0 0;
   }
 `;
 
 export const MoviesListTitle = styled.h1`
-  margin: 0 24px;
   font-size: 36px;
   font-weight: ${({ theme }) => theme.fontWeight.semiBold};
   line-height: 1.2;
