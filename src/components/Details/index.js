ad86c21c5c6dc7e6d4c4ca3cf44679fa9054d7a7
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-04-08 23:31:15 +0200

    generic Details component for MoviePage and PeoplePage

---
+++ src/components/Details/index.js
@@ -0,0 +1,103 @@
+import {
+  DetailsWrapper,
+  DetailsImage,
+  DetailsTitle,
+  DetailsSubtitle,
+  DetailsInfo,
+  DetailsExtraInfoContainer,
+  DetailsExtraInfo,
+  DetailsExtraInfoLabel,
+  DetailsDescription,
+  DetailsSection,
+  DetailsHeading,
+} from "./styled";
+import { MovieTags } from "../MovieTile/MovieTags";
+import { MovieRating } from "../MovieTile/MovieRating";
+
+export const Details = ({
+  movies,
+  imageURL,
+  poster,
+  title,
+  subtitle,
+  detailsExtraInfoTitle,
+  detailsExtraInfo,
+  detailsDateInfoTitle,
+  detailsDateInfo,
+  tags,
+  rating,
+  votes,
+  description,
+  castHeading,
+  castContent,
+  crewHeading,
+  crewContent,
+}) => (
+  <>
+    <DetailsWrapper>
+      <DetailsImage
+        src={imageURL + poster}
+        alt={`${title} ${movies ? "movies" : "person"} poster`}
+      />
+      <DetailsInfo>
+        <DetailsTitle>{title}</DetailsTitle>
+        {movies ?
+          <DetailsSubtitle>{subtitle}</DetailsSubtitle> : ""
+        }
+        <DetailsExtraInfoContainer>
+          <DetailsExtraInfo>
+            {movies ?
+              <>
+                <DetailsExtraInfoLabel>
+                  {detailsExtraInfoTitle}
+                </DetailsExtraInfoLabel>{" "}
+                {detailsExtraInfo}
+              </>
+              :
+              <>
+                <DetailsExtraInfoLabel>
+                  {detailsDateInfoTitle}
+                </DetailsExtraInfoLabel>{" "}
+                {detailsDateInfo}
+              </>
+            }
+          </DetailsExtraInfo>
+          <DetailsExtraInfo>
+            {movies ?
+              <>
+                <DetailsExtraInfoLabel>
+                  {detailsDateInfoTitle}
+                </DetailsExtraInfoLabel>{" "}
+                {detailsDateInfo}
+              </>
+              :
+              <>
+                <DetailsExtraInfoLabel>
+                  {detailsExtraInfoTitle}
+                </DetailsExtraInfoLabel>{" "}
+                {detailsExtraInfo}
+              </>
+            }
+          </DetailsExtraInfo>
+        </DetailsExtraInfoContainer>
+        {movies ?
+          <>
+            <MovieTags tags={tags ? tags.map((tag) => tag.name) : ""} />
+            <MovieRating big="true" rating={rating} votes={votes} />
+          </>
+          : ""
+        }
+
+      </DetailsInfo>
+      <DetailsDescription>{description}</DetailsDescription>
+    </DetailsWrapper>
+    <DetailsSection>
+      <DetailsHeading>{castHeading}</DetailsHeading>
+      {castContent}
+    </DetailsSection>
+    <DetailsSection>
+      <DetailsHeading>{crewHeading}</DetailsHeading>
+      {crewContent}
+    </DetailsSection>
+  </>
+);
---
+++ src/components/Details/styled.js
@@ -0,0 +1,139 @@
+import styled from "styled-components";
+
+export const DetailsWrapper = styled.article`
+  margin: 64px 24px;
+  padding: 40px;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr;
+  gap: 40px;
+  background: ${({ theme }) => theme.color.white};
+  box-shadow: 0 4px 12px rgba(186, 199, 213, 0.5);
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    gap: 16px;
+    margin: 16px;
+    padding: 16px;
+  }
+`;
+
+export const DetailsImage = styled.img`
+  max-width: 312px;
+  max-height: 464px;
+  grid-row: span 2;
+  transition: 0.6s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    max-width: 114px;
+    max-height: 169px;
+    grid-row: span 1;
+  }
+`;
+
+export const DetailsTitle = styled.h3`
+  margin: 0;
+  font-size: 36px;
+  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 16px;
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+    line-height: 1.3;
+  }
+`;
+
+export const DetailsSubtitle = styled.div`
+  font-size: 22px;
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-top: -4px;
+    font-size: 12px;
+    color: ${({ theme }) => theme.color.darkerGrey};
+    line-height: 1.3;
+  }
+`;
+
+export const DetailsInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  font-size: 18px;
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    gap: 8px;
+    font-size: 12px;
+    line-height: 1.3;
+  }
+`;
+
+export const DetailsExtraInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const DetailsExtraInfo = styled.p`
+  margin: 0;
+  display: flex;
+  gap: 10px;
+`;
+
+export const DetailsExtraInfoLabel = styled.span`
+  color: ${({ theme }) => theme.color.darkerGrey};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    display: none;
+  }
+`;
+
+export const DetailsTags = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  padding-left: 0;
+`;
+
+export const DetailsTag = styled.li`
+  margin: 0 8px;
+  padding: 8px 16px;
+  border-radius: 5px;
+  font-size: 14px;
+  background: ${({ theme }) => theme.color.mystic};
+`;
+
+export const DetailsDescription = styled.p`
+  margin: 0;
+  font-size: 20px;
+  line-height: 1.6;
+  transition: 0.6s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.descriptionChange}px) {
+    grid-column: span 2;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 14px;
+  }
+`;
+
+export const DetailsSection = styled.section`
+  margin: 0 24px 64px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    margin-bottom: 16px;
+  }
+`;
+
+export const DetailsHeading = styled.h1`
+  margin: 0 0 24px;
+  font-size: 36px;
+  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-size: 20px;
+  }
+`;
