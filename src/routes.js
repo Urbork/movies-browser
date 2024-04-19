272ea0dc8c5b75462a70cec7ed426b14354c7e3d
Adrian Wozniak <adrianwozniak20@gmail.com>
2024-04-19 18:34:08 +0200

    basic paths for movie and people lists and details, w/o pages

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,5 @@
-export const toMoviesList = ({ page } = { page: ":page" }) => `/moviesList/${page}`;
-export const toPeopleList = ({ page } = { page: ":page" }) => `/peopleList/${page}`;
+export const toMoviesList = () => `/movies`;
+export const toPeopleList = () => `/people`;
 
-export const toMovieDetails = ({ id } = { id: ":id" }) => `/moviesList/movieDetails/${id}`;
-export const toPeopleDetails = ({ id } = { id: ":id" }) => `/peopleList/peopleDetails/${id}`;
-/*
-ścieżka /moviesList/movieDetails/... jest chwilowa. dałem w ten sposób, bo jeśli usunie się z niej movieDetails/
-to strona automatycznie ładuje tylko szczegóły, a nie przechodzi wogóle do list. Możliwe że problem się
-rozwiąże jak ogarniemy dodanie numeru strony do URLa, ale chwilowo mi to nie działa
-*/
+export const toMovieDetails = ({ id } = { id: ":id" }) => `/movies/id=${id}`;
+export const toPeopleDetails = ({ id } = { id: ":id" }) => `/people/id=${id}`;
