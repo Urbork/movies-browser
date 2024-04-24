d215fd8bb45f80fcc2c342f5ac750e5bdd13d22b
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-04-24 19:11:15 +0200

    Removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { Navigation } from "./components/Navigation";
-import { Pagination } from "./components/Pagination";
 import { Container } from "./components/Container";
 import { MovieDetails } from "./features/MovieDetails";
 import { MoviesList } from "./features/MoviesList";
 import { PeopleDetails } from "./features/PeopleDetails";
 import PeopleList from "./components/PeopleList";
-import { LoadingPage } from "./components/LoadingPage";
-import { ErrorPage } from "./components/ErrorPage";
 import { useSelector } from "react-redux";
-import { selectFetchStatus, selectFirstMoviePage } from "./features/pageState/pageStateSlice";
+import { selectFirstMoviePage } from "./features/pageState/pageStateSlice";
 import { toMovieDetails, toMoviesList, toPeopleDetails, toPeopleList } from "./routes";
 
 function App() {
-  const fetchStatus = useSelector(selectFetchStatus);
   const firstMoviePage = useSelector(selectFirstMoviePage);
 
   return (
