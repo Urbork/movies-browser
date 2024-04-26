88097c0d0027e15e787720db17d9b5470bfd1048
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-04-26 09:28:36 +0200

    test

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,28 +1,39 @@
 import { call, delay, put, select, takeLatest } from "redux-saga/effects";
-import { getPopularPeople } from "../../api/fetchApi";
-import { setPopularPeople } from "./peopleSlice";
+import { getPopularPeople, getSearchPerson } from "../../api/fetchApi";
+import { setPopularPeople, setSearchPerson } from "./peopleSlice";
 import {
-  fetchApi, fetchError,
+  fetchApi,
+  fetchError,
   resetFetchStatus,
   selectCurrentPeoplePage,
+  selectQuery,
   setCurrentPeoplePage,
   setImagesToLoad,
+  setQuery,
 } from "../pageState/pageStateSlice";
 
 function* fetchApiHandler() {
   try {
     yield put(fetchApi());
     const page = yield select(selectCurrentPeoplePage);
-    const people = yield call(() => getPopularPeople(page));
-    yield delay(1000);
+    const query = yield select(selectQuery);
+    let people;
+    if (query) {
+      people = yield call(() => getSearchPerson(query, page));
+      yield delay(1000);
+      yield put(setSearchPerson(people.results));
+    } else {
+      people = yield call(() => getPopularPeople(page));
+      yield delay(1000);
+    }
     yield put(setPopularPeople(people.results));
     yield put(setImagesToLoad());
     yield put(resetFetchStatus());
   } catch (error) {
     yield put(fetchError());
   }
-};
+}
 
 export function* peopleSaga() {
-  yield takeLatest(setCurrentPeoplePage.type, fetchApiHandler);
-};
+  // yield takeLatest([setCurrentPeoplePage.type, setQuery.type], fetchApiHandler);
+}
